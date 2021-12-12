123d735a6a516f4ba7fa653ad33412513b5c77ac
Patronum129 <diego88rising@gmail.com>
2021-12-12 23:20:33 +0100

    finished

--- Game/resource.h
+++ Game/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Archivo de inclusión generado de Microsoft Visual C++.
-// Usado por Game.rc
+// Microsoft Visual C++ generated include file.
+// Used by Game.rc
 //
 #define IDI_ICON1                       101
 
